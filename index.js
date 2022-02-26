512fa4276881828431379d6faa4be60c20bb4674
Pascal Weiland <weiland@users.noreply.github.com>
2022-02-26 22:10:59 +0100

    :octocat: Initial commit
    Create basic prettier config.
    Since we target modern prettier, most defaults are good enough.

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100, // non-default (prettier recommends 80)
+  tabWidth: 2, // default
+  useTabs: false, // default
+  semi: false, // default
+  singleQuote: true,
+  quoteProps: 'as-needed', // default
+  jsxSingleQuote: false, // default
+  trailingComma: 'es5', // default since 2.0.0
+  bracketSpacing: true, // default
+  bracketSameLine: false, // default
+  arrowParens: 'always', // default since 2.0.0
+  endOfLine: 'lf', // default since 2.0.0
+};
---
+++ readme.md
@@ -0,0 +1,29 @@
+# @weiland's prettier config
+
+## Install the npm package
+
+```command
+pnpm add -D @weiland/prettier-config
+```
+
+or with _yarn_:
+
+```command
+yarn add -D @weiland/prettier-config
+```
+
+## Add to `package.json`
+
+```json
+{
+  "prettier": "@weiland/prettier-config"
+}
+```
+
+or create a `.prettierrc.json` file with following contents:
+
+```json
+"@weiland/prettier-config"
+```
+
+Also make sure to have `prettier` installed!
