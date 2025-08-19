8db7295e3aa744eb8132a1adba9ab3573247a964
Pascal Weiland <weiland@users.noreply.github.com>
2025-08-19 18:26:45 +0200

    docs: clean up

--- index.js
+++ index.js
@@ -6,10 +6,9 @@
  */
 const overridableDefaults = {
 	endOfLine: 'lf',
-	// tabWidth is neither set in .editorconfig nor here, so every developer
-	// can set their own preference in their editor locally.
 	// tabWidth: 4, // defaults to `2`
-	// default is 80
+	// `tabWidth` is neither set in `.editorconfig` nor here, so every developer
+	// can set their own preference in their editor locally.
 	// same goes with `printWidth`. We inherit this value from the editorconfig's `max_line_length`.
 	// printWidth: 120, // prettier recommends 80 (and reminds that this is not a hard-limit (like `max-len` in ESLint).
 }
